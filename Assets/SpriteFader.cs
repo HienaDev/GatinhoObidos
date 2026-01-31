89863c262eaa7d622e3bc89a68819314b6680f17
HienaDev <ant.rodrigues1999@gmail.com>
2026-01-31 12:41:41 +0000

    New sounds

--- Assets/SpriteFader.cs
+++ Assets/SpriteFader.cs
@@ -23,7 +23,7 @@ public class SpriteFader : MonoBehaviour
         spriteRenderer.color = new Color(spriteRenderer.color.r, spriteRenderer.color.g, spriteRenderer.color.b, 1f); // Ensure starting alpha is 1
 
         audioSource = gameObject.AddComponent<AudioSource>();
-        audioSource.volume = 0.55f;
+        audioSource.volume = 0.2f;
         audioSource.outputAudioMixerGroup = audioMixer;
     }
 
